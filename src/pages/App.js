c4bbacbe3b092434e864eb6c932eba7b2dad4f56
Jair Costa <juniorceia1996@gmail.com>
2021-08-24 14:51:52 -0300

    criação do designe do projeto pokedex

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import Pokedesk from '../components/pokedesk';
+import './app.css'
+
+function App() {
+  return (
+    <div>
+      <h1>
+       <Pokedesk />
+      </h1>
+     
+    </div>
+  );
+}
+
+export default App;
